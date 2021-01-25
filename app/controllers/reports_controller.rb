2358b55f398def3c4aca07bec3da681e685e4f35
Mr-DareDevil <arnabsen1729@gmail.com>
2021-01-26 02:27:31 +0530

    Report Added

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,6 +3,7 @@ class ReportsController < ApplicationController
     @from_date = params[:from_date]
     @to_date = params[:to_date]
     @orders = Order.delivered.range(@from_date, @to_date)
+    @total_price = @orders.map { |order| order.get_price }.sum
     render "reports/index"
   end
 
